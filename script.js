e9d6fe873b2d8580d2c2dfaae30bb65fa18bf142
mczekajski <czekajskimarcin@gmail.com>
2020-10-20 19:55:26 +0200

    add jump run stand

--- script.js
+++ script.js
@@ -12,7 +12,8 @@ let lastAnimationUpdate = date.getTime();
 class Game {
     constructor() {
         this.date = new Date();
-        this.standingFrame = 1;
+        this.frame = 1;
+        this.frameCounter = 0;
     }
 
     drawBottomLine() {
@@ -31,7 +32,7 @@ class Game {
             case "jump":
                 this.drawPlayerJumping();
                 break;
-            default: 
+            default:
                 this.drawPlayerStanding();
         }
     }
@@ -41,21 +42,44 @@ class Game {
     }
 
     drawPlayerStanding() {
-        // console.log("Player is standing!");
+        console.log("Player is standing!");
         this.date = new Date();
-        console.log(this.date.getMilliseconds());
-        (this.date.getMilliseconds() > 500) == 0 ? this.standingFrame = 2 : this.standingFrame = 1;
-        player.image.src = `img/char/standing/frame-${this.standingFrame}.png`;
-        ctx.drawImage(player.image, 300, canvas.height - player.image.height - 100);
+        (this.date.getMilliseconds() > 500) ? this.frame = 2: this.frame = 1;
+        player.image.src = `img/char/standing/frame-${this.frame}.png`;
+        ctx.drawImage(player.image, canvas.width/2 - player.image.width/2, canvas.height - player.image.height - 100);
     }
 
     drawPlayerRunning() {
         console.log("Player is running!");
+        this.date = new Date();
+        if (this.frameCounter > 3) {
+            this.frameCounter = 0;
+            if (this.frame < 5) {
+                this.frame++;
+            } 
+            else {
+                this.frame = 1;
+            }
+        }
+        else {
+            this.frameCounter++;
+        }
+
+        player.image.src = `img/char/run/frame-${this.frame}.png`;
+        ctx.drawImage(player.image, canvas.width/2 - player.image.width/2, canvas.height - player.image.height - 100);
     }
 
     drawPlayerJumping() {
         console.log("Player is jumping!");
-        player.activity = "stand";
+        player.currentJumpHeight += player.jumpSpeed;
+        player.jumpSpeed -= 2;
+        player.image.src = `img/char/jump/jump-up.png`;
+        ctx.drawImage(player.image, canvas.width/2 - player.image.width/2, canvas.height - player.image.height - 100 - player.currentJumpHeight);
+        if (player.currentJumpHeight < 0) {
+            player.jumpSpeed = 20;
+            player.currentJumpHeight = 0;
+            player.activity = "stand";
+        }
     }
 }
 
@@ -66,6 +90,7 @@ class Player {
         this.animationSpeed = animationSpeed;
         this.image = new Image();
         this.image.src = 'img/char/standing/frame-1.png';
+        this.currentJumpHeight = 0;
     }
 
     stand() {
@@ -73,10 +98,12 @@ class Player {
     }
 
     run() {
+        this.frameCounter = 0;
         this.activity = "run";
     }
 
     jump() {
+        this.frameCounter = 0;
         this.activity = "jump";
     }
 
@@ -95,11 +122,12 @@ function animate() {
     game.drawPlayer();
 }
 
-window.addEventListener('load', animate);
 window.addEventListener('keydown', e => {
-    if (e.keyCode === 32) player.jump();
+    if (e.keyCode === 32 && player.activity != "jump") player.jump();
     if (e.keyCode === 39) player.run();
 });
 window.addEventListener('keyup', e => {
     if (e.keyCode === 39) player.stand();
 });
+
+window.addEventListener('load', animate);
